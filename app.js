82cdd84600d619434d82ab08a8aeaa5b2a83cd1a
shouko <imshouko@gmail.com>
2020-05-22 01:34:40 +0900

    Use Express

--- app.js
+++ app.js
@@ -1,112 +1,86 @@
 /* eslint-disable no-console */
-const http = require('http');
 const fetch = require('node-fetch');
-const mimes = require('mime-types');
-const etag = require('etag');
+const express = require('express');
+const bodyParser = require('body-parser');
+const mime = require('mime-types');
 const config = require('./config');
 
-const corsHeaders = { 'access-control-allow-origin': config.allowOrigin };
+const app = express();
+app.disable('x-powered-by');
 
-const send = (res, code, msg) => {
-  res.writeHead(code, corsHeaders);
-  res.end(msg || '');
-};
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.header('access-control-allow-origin', config.allowOrigin);
+  return next();
+});
 
-const server = http.createServer(async (req, res) => {
-  if (req.method === 'OPTIONS') {
-    return send(res, 204);
+app.get('/', (req, res) => res.send('Hello World!\n'));
+
+app.options((req, res) => res.sendStatus(204));
+
+app.post('/*', async (req, res) => {
+  try {
+    if (
+      !req.body.username
+      || !req.body.token
+      || !req.headers['x-api-version']
+      || !req.headers['x-user-agent']
+    ) throw new Error();
+
+    const resp = await fetch(`${config.upstream}${req.url.split('?')[0]}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-API-Version': req.headers['x-api-version'],
+        Accept: '*/*',
+        'User-Agent': req.headers['x-user-agent'],
+      },
+      body: JSON.stringify(req.body),
+    }).then((r) => r.text());
+
+    res.contentType('application/json; charset=utf-8');
+    return res.send(resp);
+  } catch (e) {
+    console.error(e);
+    return res.sendStatus(400);
+  }
+});
+
+app.get('/media/:host/*', async (req, res) => {
+  const { host } = req.params;
+  const rawQuery = req.originalUrl.split('?')[1] || '';
+  const path = req.params[0];
+  let fullPath = path;
+  if (rawQuery.length > 0) {
+    fullPath += `?${encodeURI(rawQuery)}`;
   }
 
-  if (req.method === 'POST') {
-    try {
-      req.body = await new Promise((resolve, reject) => {
-        let data = '';
-        req.on('data', (chunk) => {
-          data += chunk;
-          if (data.length > 1024 * 1024) reject();
-        }).on('end', () => {
-          try {
-            resolve(JSON.parse(data.toString()));
-          } catch (e) {
-            reject(e);
-          }
-        });
-      });
-
-      if (
-        !req.body.username
-        || !req.body.token
-        || !req.headers['x-api-version']
-        || !req.headers['x-user-agent']
-      ) throw new Error();
-
-      const resp = await fetch(`${config.upstream}${req.url.split('?')[0]}`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'X-API-Version': req.headers['x-api-version'],
-          Accept: '*/*',
-          'User-Agent': req.headers['x-user-agent'],
-        },
-        body: JSON.stringify(req.body),
-      }).then((r) => r.text());
-
-      res.writeHead(200, {
-        ...corsHeaders,
-        'content-type': 'application/json; charset=utf-8',
-      });
-      return res.end(resp);
-    } catch (e) {
-      console.error(e);
-      return send(res, 400);
-    }
-  }
-
-  if (req.method !== 'GET') {
-    return send(res, 405);
-  }
-
-  if (req.url.split('?')[0] === '/') {
-    return send(res, 200, 'Hello World\n');
-  }
-
-  if (!req.url.startsWith('/media/')) {
-    return send(res, 404);
-  }
-
-  const [,, host, ...path] = req.url.split('/');
   if (!config.mediaUpstreams.some((d) => host.endsWith(d))) {
-    return send(res, 400);
+    return res.sendStatus(400);
   }
 
   // TODO: cache
   try {
-    const body = await fetch(`https://${host}/${path.join('/')}`)
+    const body = await fetch(`https://${host}/${fullPath}`)
       .then((resp) => resp.blob())
       .then((blob) => blob.arrayBuffer())
       .then((abuff) => Buffer.from(abuff, 'binary'));
 
-    const contentType = mimes.lookup(req.url.split('?')[0]) || 'application/octet-stream';
-
-    res.writeHead(200, {
-      ...corsHeaders,
-      etag: etag(body),
-      'content-type': (() => {
-        if (['image/', 'video/'].some((prefix) => contentType.startsWith(prefix))) {
-          return contentType;
-        }
-        return 'application/octet-stream';
-      })(),
-    });
-    return res.end(body);
+    const contentType = mime.lookup(path);
+    const allowedMimes = ['image/', 'video/'];
+    if (allowedMimes.some((m) => contentType.startsWith(m))) {
+      res.contentType(contentType);
+    } else {
+      res.contentType('application/octet-stream');
+    }
+    return res.send(body);
   } catch (e) {
     console.error(e);
-    send(res, 500);
   }
 
-  return send(res, 500);
+  return res.sendStatus(500);
 });
 
-const listener = server.listen(config.port || 3000, () => {
-  console.log(listener.address().port);
+const listener = app.listen(config.port, () => {
+  console.log(`Listening on port ${listener.address().port}!`);
 });
